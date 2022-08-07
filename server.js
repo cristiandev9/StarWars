7c7cb632faad1ad1a3ac1f137e29f1c8d7230bb7
cristian <cristian.andrisen@gmail.com>
2022-08-07 10:17:56 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const { resolve } = require('path')
+
+const app = express()
+
+
+app.use('/', express.static(
+  resolve(
+    __dirname,
+    './build'
+  )
+))
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+  
+
+  console.log('Tudo funcionando')
+})
